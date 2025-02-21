b87c66297d751a2fbec835b6f541d9d8518a8aea
gr1ckly <yarm445@gmail.com>
2025-02-21 19:53:30 +0300

    feat: added random generation matrix

--- Lab_1/main.go
+++ Lab_1/main.go
@@ -4,15 +4,18 @@ import (
 	matrix2 "Lab_1/matrix"
 	"bufio"
 	"fmt"
+	"math/rand"
 	"os"
 	"strconv"
 	"strings"
+	"time"
 )
 
 func main() {
 	var size, maxIter int
 	var accuracy float64
 	var isFile bool
+	var isRandom bool
 	reader, file, readerErr := getReader()
 	if file != nil {
 		defer file.Close()
@@ -38,48 +41,70 @@ func main() {
 	if err != nil {
 		fmt.Println(err.Error())
 	}
-	if !isFile {
-		fmt.Println("Введите матрицу:")
-	}
 	data := make([][]float64, size)
-	for i := 0; i < size; i++ {
-		data[i] = make([]float64, size)
+	vecB := make([]float64, size)
+	if !isFile {
+		fmt.Print("Введите любой символ если хотите заполнить матрицу случайными числами: ")
+		line, err = reader.ReadString('\n')
+		line = line[:len(line)-1]
+		if line != "" {
+			rand.Seed(time.Now().UnixNano())
+			for i := 0; i < size; i++ {
+				data[i] = make([]float64, size)
+				for j := 0; j < size; j++ {
+					data[i][j] = rand.Float64() * 100
+				}
+			}
+			for i := 0; i < size; i++ {
+				vecB[i] = rand.Float64()
+			}
+			isRandom = true
+		} else {
+			isRandom = false
+		}
+	}
+	if !isRandom {
+		if !isFile {
+			fmt.Println("Введите матрицу:")
+		}
+		for i := 0; i < size; i++ {
+			data[i] = make([]float64, size)
+			line, err = reader.ReadString('\n')
+			if err != nil {
+				fmt.Println(err.Error())
+				return
+			}
+			splitline = strings.Split(strings.TrimSpace(line), " ")
+			if len(splitline) != size {
+				fmt.Printf("Длина %d строки матрицы некорректна\n", i+1)
+			}
+			for j := 0; j < size; j++ {
+				data[i][j], err = strconv.ParseFloat(splitline[j], 64)
+				if err != nil {
+					fmt.Println(err.Error())
+					return
+				}
+			}
+		}
+		if !isFile {
+			fmt.Println("Введите вектор b: ")
+		}
 		line, err = reader.ReadString('\n')
 		if err != nil {
 			fmt.Println(err.Error())
 			return
 		}
 		splitline = strings.Split(strings.TrimSpace(line), " ")
-п		if len(splitline) != size {
-			fmt.Printf("Длина %d строки матрицы некорректна\n", i+1)
+		if len(splitline) != size {
+			fmt.Println("Некорректное количество элементов для вектора b")
+			return
 		}
-		for j := 0; j < size; j++ {
-			data[i][j], err = strconv.ParseFloat(splitline[j], 64)
+		for i := 0; i < size; i++ {
+			vecB[i], err = strconv.ParseFloat(splitline[i], 64)
 			if err != nil {
 				fmt.Println(err.Error())
 				return
 			}
-		}
-	}
-	if !isFile {
-		fmt.Println("Введите вектор b: ")
-	}
-	line, err = reader.ReadString('\n')
-	if err != nil {
-		fmt.Println(err.Error())
-		return
-	}
-	splitline = strings.Split(strings.TrimSpace(line), " ")
-	if len(splitline) != size {
-		fmt.Println("Некорректное количество элементов для вектора b")
-		return
-	}
-	vecB := make([]float64, size)
-	for i := 0; i < size; i++ {
-		vecB[i], err = strconv.ParseFloat(splitline[i], 64)
-		if err != nil {
-			fmt.Println(err.Error())
-			return
 		}
 	}
 	if !isFile {
@@ -109,6 +134,10 @@ func main() {
 		return
 	}
 	matrix := matrix2.BuildMatrix(size, maxIter, data, vecB, accuracy)
+	if isRandom {
+		fmt.Println("Сгенерированная матрица: ")
+		fmt.Println(matrix.StringMatrix())
+	}
 	if matrix.MakeDiagonallyDominant() {
 		fmt.Println("Диагональное преобразование найдено:")
 		fmt.Println(matrix.StringMatrix())
